efbe40abbf6092ed7e9f9d8b605c6f21ac21b3a6
𝐶𝑆𝑌𝑂𝑁 <134310111+CSY0N@users.noreply.github.com>
2025-11-24 14:31:39 +0100

    Delete src/core directory

--- src/core/frontend/manager/styles/Themes.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-namespace YimMenu
-{
-	// Forward declarations of each theme's render function, append when adding new themes
-	extern void RenderClassicTheme();
-	extern void RenderModernTheme();
-	extern void RenderModernVTheme();
-}
