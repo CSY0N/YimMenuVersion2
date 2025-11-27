e278c27dc311fa9ea34e021f11a79de2c24b419f
lootte <deede3771@gmail.com>
2025-11-26 21:09:49 -0500

    added Beast Jump code

--- src/game/features/self/BeastJump.cpp
+++ src/game/features/self/BeastJump.cpp
@@ -1,1 +1,21 @@
+#include "core/commands/LoopedCommand.hpp"
+#include "game/backend/Self.hpp"
+#include "game/gta/Natives.hpp"
+
+namespace YimMenu::Features
+{
+	class BeastJump : public LoopedCommand
+	{
+		using LoopedCommand::LoopedCommand;
+
+		virtual void OnTick() override
+		{
+			if (PAD::IS_CONTROL_JUST_PRESSED(0, 22) && !PED::IS_PED_IN_ANY_VEHICLE(PLAYER::PLAYER_PED_ID(), NULL) && !ENTITY::IS_ENTITY_IN_AIR(PLAYER::PLAYER_PED_ID()))
+			{
+				TASK::TASK_JUMP(PLAYER::PLAYER_PED_ID(), true, true, true);
+			}
+		}
+	};
+	static BeastJump _BeastJump{"beastjump", "Beast Jump", "Extreme forward + vertical jump force"};
+}
 
