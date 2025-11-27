83845076343538a2894dda739a457c6a8f03d2ad
lootte <deede3771@gmail.com>
2025-11-26 21:11:10 -0500

    added Swim in air code
    Implement SwimInAir feature allowing aerial movement.

--- src/game/features/self/SwimInAir.cpp
+++ src/game/features/self/SwimInAir.cpp
@@ -1,1 +1,24 @@
+#include "core/commands/LoopedCommand.hpp"
+#include "game/backend/Self.hpp"
+#include "game/gta/Natives.hpp"
 
+namespace YimMenu::Features
+{
+	class SwimInAir : public LoopedCommand
+	{
+		using LoopedCommand::LoopedCommand;
+
+		virtual void OnTick() override
+		{
+			PED::SET_PED_CONFIG_FLAG(PLAYER::PLAYER_PED_ID(), 65, true);
+		}
+		virtual void OnDisable() override
+		{
+			PED::SET_PED_CONFIG_FLAG(PLAYER::PLAYER_PED_ID(), 65, false);
+		}
+	};
+	static SwimInAir _SwimInAir{
+	    "swiminair",
+	    "Swim in Air",
+	    "Allows swimming and movement while in the air"};
+}
