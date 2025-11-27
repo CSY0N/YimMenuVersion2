05df899e111145c14e711c4e6f80d9ee5d08e2fd
lootte <deede3771@gmail.com>
2025-11-26 21:08:58 -0500

    added slide run code

--- src/game/features/self/SlideRun.cpp
+++ src/game/features/self/SlideRun.cpp
@@ -1,1 +1,39 @@
+#include "core/commands/LoopedCommand.hpp"
+#include "game/backend/Self.hpp"
+#include "game/gta/Natives.hpp"
+
+namespace YimMenu::Features
+{
+	class SlideRun : public LoopedCommand
+	{
+		using LoopedCommand::LoopedCommand;
+
+		virtual void OnTick() override
+		{
+			if (TASK::IS_PED_RUNNING(PLAYER::PLAYER_PED_ID()) || TASK::IS_PED_SPRINTING(PLAYER::PLAYER_PED_ID()))
+			{
+				ENTITY::APPLY_FORCE_TO_ENTITY(
+				    PLAYER::PLAYER_PED_ID(),
+				    1,
+				    0.0f,
+				    1.5f,
+				    0.0f,
+				    0.0f,
+				    0.0f,
+				    0.0f,
+				    0,
+				    TRUE,
+				    TRUE,
+				    TRUE,
+				    FALSE,
+				    TRUE
+				);
+			}
+		}
+	};
+	static SlideRun _SlideRun{
+	    "sliderun",
+	    "Slide Run",
+	    "Break the Speed Limit"};
+}
 
