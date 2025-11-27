d290d1cd18c1f132ab25a10df5225083135529d2
lootte <deede3771@gmail.com>
2025-11-26 21:13:37 -0500

    added Slow Montion code
    Implements SlowMotion feature to alter game time scale.

--- src/game/features/self/SlowMotion.cpp
+++ src/game/features/self/SlowMotion.cpp
@@ -1,1 +1,24 @@
+#include "core/commands/LoopedCommand.hpp"
+#include "game/gta/Natives.hpp"
+
+namespace YimMenu::Features
+{
+	class SlowMotion : public LoopedCommand
+	{
+		using LoopedCommand::LoopedCommand;
+
+		virtual void OnTick() override
+		{
+			MISC::SET_TIME_SCALE(0.3f);
+		}
+		virtual void OnDisable() override
+		{
+			MISC::SET_TIME_SCALE(1.0f);
+		}
+	};
+	static SlowMotion _SlowMotion{
+	    "slowmotion",
+	    "Slow Motion",
+	    "Slows down the entire game world for cinematic effects"};
+}
 
