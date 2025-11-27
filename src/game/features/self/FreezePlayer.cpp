189d33b606a0a5160c882fb01dcba6734dc33cf7
lootte <deede3771@gmail.com>
2025-11-26 21:11:48 -0500

    added Freeze Player code

--- src/game/features/self/FreezePlayer.cpp
+++ src/game/features/self/FreezePlayer.cpp
@@ -1,1 +1,21 @@
+#include "core/commands/LoopedCommand.hpp"
+#include "game/backend/Self.hpp"
+#include "game/gta/Natives.hpp"
+
+namespace YimMenu::Features
+{
+	class FreezePlayer : public LoopedCommand
+	{
+		using LoopedCommand::LoopedCommand;
+		virtual void OnTick() override
+		{
+			ENTITY::FREEZE_ENTITY_POSITION(PLAYER::PLAYER_PED_ID(), true);
+		}
+		virtual void OnDisable() override
+		{
+			ENTITY::FREEZE_ENTITY_POSITION(PLAYER::PLAYER_PED_ID(), false);
+		}
+	};
+	static FreezePlayer _FreezePlayer{"freezeplayer", "Freeze Player", "Completely lock your position in place"};
+}
 
