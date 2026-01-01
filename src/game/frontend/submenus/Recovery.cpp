4c04b9244b94f739aee7620c21f5ed754fcc2e08
TickerDOT <105274387+TickerDOT@users.noreply.github.com>
2026-01-01 20:10:30 +0600

    Update Recovery.cpp
    Added
    
    1. Remove Gunvan Weapon (If Set)
    2. Weapon Skins For Knife and Baseball Bat
    3. GunVan Blip Always Visible
    4. Teleport To GunVan
    5. Gun Van Set Location

--- src/game/frontend/submenus/Recovery.cpp
+++ src/game/frontend/submenus/Recovery.cpp
@@ -11,69 +11,75 @@ namespace YimMenu::Submenus
 		#define ICON_FA_SACK_DOLLAR "\xef\x93\x80"
 	    Submenu::Submenu("Recovery", ICON_FA_SACK_DOLLAR)
 	{
-		auto main = std::make_shared<Category>("Main");
-		auto businesses = std::make_shared<Category>("Businesses");
-		auto casino = std::make_shared<Category>("Casino");
-		auto unlocks = std::make_shared<Category>("Unlocks");
-		auto generalUnlocks = std::make_shared<Group>("General");
-		auto trophies = std::make_shared<Group>("Trophies");
-		auto skip = std::make_shared<Group>("Skipper");
+		auto main = std::make_shared("Main");
+		auto businesses = std::make_shared("Businesses");
+		auto casino = std::make_shared("Casino");
+		auto unlocks = std::make_shared("Unlocks");
+		auto generalUnlocks = std::make_shared("General");
+		auto trophies = std::make_shared("Trophies");
+		auto skip = std::make_shared("Skipper");
 
-		auto generalGroup = std::make_shared<Group>("General");
-		auto gunvanGroup = std::make_shared<Group>("Gun Van");
-		auto businessGroup = std::make_shared<Group>("General");
-		auto casinoSlots = std::make_shared<Group>("Slot Machines");
-		//auto casinoWheel = std::make_shared<Group>("Lucky Wheel");
-		//auto casinoBlackJack = std::make_shared<Group>("Blackjack");
-		//auto casinoRoulette = std::make_shared<Group>("Roulette");
+		auto generalGroup = std::make_shared("General");
+		auto gunvanGroup = std::make_shared("Gun Van Accessories");
+		auto businessGroup = std::make_shared("General");
+		auto casinoSlots = std::make_shared("Slot Machines");
+		//auto casinoWheel = std::make_shared("Lucky Wheel");
+		//auto casinoBlackJack = std::make_shared("Blackjack");
+		//auto casinoRoulette = std::make_shared("Roulette");
 
-		generalGroup->AddItem(std::make_shared<BoolCommandItem>("playallmissionssolo"_J));
-		generalGroup->AddItem(std::make_shared<BoolCommandItem>("unlockgtaplus"_J));
-		generalGroup->AddItem(std::make_shared<BoolCommandItem>("overriderpmultiplier"_J));
-		generalGroup->AddItem(std::make_shared<ConditionalItem>("overriderpmultiplier"_J, std::make_shared<FloatCommandItem>("rpmultiplierinput"_J)));
-		generalGroup->AddItem(std::make_shared<CommandItem>("maxskills"_J));
-		generalGroup->AddItem(std::make_shared<IntCommandItem>("rpslider"_J, "Character Level"));
-		generalGroup->AddItem(std::make_shared<CommandItem>("applyrp"_J));
-		generalGroup->AddItem(std::make_shared<IntCommandItem>("lscarmeetrank"_J, "LS Car Meet Rank"));
-		generalGroup->AddItem(std::make_shared<CommandItem>("applylscarmeetrank"_J));
+		generalGroup->AddItem(std::make_shared("playallmissionssolo"_J));
+		generalGroup->AddItem(std::make_shared("unlockgtaplus"_J));
+		generalGroup->AddItem(std::make_shared("overriderpmultiplier"_J));
+		generalGroup->AddItem(std::make_shared("overriderpmultiplier"_J, std::make_shared("rpmultiplierinput"_J)));
+		generalGroup->AddItem(std::make_shared("maxskills"_J));
+		generalGroup->AddItem(std::make_shared("rpslider"_J, "Character Level"));
+		generalGroup->AddItem(std::make_shared("applyrp"_J));
+		generalGroup->AddItem(std::make_shared("lscarmeetrank"_J, "LS Car Meet Rank"));
+		generalGroup->AddItem(std::make_shared("applylscarmeetrank"_J));
 
-		generalGroup->AddItem(std::make_shared<BoolCommandItem>("freechangeappearance"_J));
-		generalGroup->AddItem(std::make_shared<BoolCommandItem>("nochangeappearancecooldown"_J));
-		generalGroup->AddItem(std::make_shared<BoolCommandItem>("allowgenderchange"_J));
-		generalGroup->AddItem(std::make_shared<BoolCommandItem>("peddropcash"_J));
-		generalGroup->AddItem(std::make_shared<BoolCommandItem>("vehicledropcash"_J));
+		generalGroup->AddItem(std::make_shared("freechangeappearance"_J));
+		generalGroup->AddItem(std::make_shared("nochangeappearancecooldown"_J));
+		generalGroup->AddItem(std::make_shared("allowgenderchange"_J));
+		generalGroup->AddItem(std::make_shared("peddropcash"_J));
+		generalGroup->AddItem(std::make_shared("vehicledropcash"_J));
 
-		
-		gunvanGroup->AddItem(std::make_shared<ListCommandItem>("gunvanslot"_J));
-		gunvanGroup->AddItem(std::make_shared<ListCommandItem>("gunvanweapon"_J));
-		gunvanGroup->AddItem(std::make_shared<CommandItem>("setgunvanweapon"_J));
+		gunvanGroup->AddItem(std::make_shared("gunvanslot"_J));
 
-		businessGroup->AddItem(std::make_shared<ListCommandItem>("businesssafe"_J));
-		businessGroup->AddItem(std::make_shared<CommandItem>("claimsafeearnings"_J));
+		auto gunvanWeaponRow = std::make_shared("", 3);
+		gunvanWeaponRow->AddItem(std::make_shared("gunvanweapon"_J));
+		gunvanWeaponRow->AddItem(std::make_shared("setgunvanweapon"_J));
+		gunvanWeaponRow->AddItem(std::make_shared("removegunvanweapon"_J));
+		gunvanGroup->AddItem(std::move(gunvanWeaponRow));
 
-		casinoSlots->AddItem(std::make_shared<BoolCommandItem>("casinomanipulaterigslotmachines"_J));
-		
-		generalUnlocks->AddItem(std::make_shared<CommandItem>("unlockallcareerprogress"_J));
-		generalUnlocks->AddItem(std::make_shared<CommandItem>("unlockalltattoos"_J));
-		generalUnlocks->AddItem(std::make_shared<CommandItem>("BunkerUnlocks"_J));
-		generalUnlocks->AddItem(std::make_shared<CommandItem>("unlockallAwards"_J));
-		generalUnlocks->AddItem(std::make_shared<CommandItem>("unlock_paints"_J));
-		generalUnlocks->AddItem(std::make_shared<CommandItem>("unlock_packed_bools"_J));
-		generalUnlocks->AddItem(std::make_shared<CommandItem>("unlock_misc"_J));
+		auto gunvanLocationRow = std::make_shared("", 2);
+		gunvanLocationRow->AddItem(std::make_shared("gunvanlocation"_J));
+		gunvanLocationRow->AddItem(std::make_shared("setgunvanlocation"_J));
+		gunvanGroup->AddItem(std::move(gunvanLocationRow));
 
-		skip->AddItem(std::make_shared<CommandItem>("SkipDrDreeContract"_J));
-		skip->AddItem(std::make_shared<CommandItem>("SkipJenetteTheMutette"_J));
-		skip->AddItem(std::make_shared<CommandItem>("SkipCluckinBellRaid"_J));
+		gunvanGroup->AddItem(std::make_shared("teleportgunvan"_J));
 
+		gunvanGroup->AddItem(std::make_shared("gunvanskins"_J));
+		gunvanGroup->AddItem(std::make_shared("gunvanblip"_J));
 
-		trophies->AddItem(std::make_shared<CommandItem>("unlockallmansiontrophies"_J));
+		businessGroup->AddItem(std::make_shared("businesssafe"_J));
+		businessGroup->AddItem(std::make_shared("claimsafeearnings"_J));
 
-		
+		casinoSlots->AddItem(std::make_shared("casinomanipulaterigslotmachines"_J));
 
+		generalUnlocks->AddItem(std::make_shared("unlockallcareerprogress"_J));
+		generalUnlocks->AddItem(std::make_shared("unlockalltattoos"_J));
+		generalUnlocks->AddItem(std::make_shared("BunkerUnlocks"_J));
+		generalUnlocks->AddItem(std::make_shared("unlockallAwards"_J));
+		generalUnlocks->AddItem(std::make_shared("unlock_paints"_J));
+		generalUnlocks->AddItem(std::make_shared("unlock_packed_bools"_J));
+		generalUnlocks->AddItem(std::make_shared("unlock_misc"_J));
 
+		skip->AddItem(std::make_shared("SkipDrDreeContract"_J));
+		skip->AddItem(std::make_shared("SkipJenetteTheMutette"_J));
+		skip->AddItem(std::make_shared("SkipCluckinBellRaid"_J));
 
+		trophies->AddItem(std::make_shared("unlockallmansiontrophies"_J));
 
-		
 		main->AddItem(generalGroup);
 		main->AddItem(gunvanGroup);
 		businesses->AddItem(businessGroup);
@@ -82,7 +88,6 @@ namespace YimMenu::Submenus
 		unlocks->AddItem(trophies);
 		unlocks->AddItem(skip);
 
-		
 		AddCategory(std::move(main));
 		AddCategory(std::move(businesses));
 		AddCategory(std::move(casino));
