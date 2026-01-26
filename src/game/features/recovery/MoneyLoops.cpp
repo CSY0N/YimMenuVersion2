b2794c8e2757fa23379ad96ba30de167446f22e5
Hexlane <deede3771@gmail.com>
2026-01-26 07:16:02 -0500

    Update MoneyLoops.cpp

--- src/game/features/recovery/MoneyLoops.cpp
+++ src/game/features/recovery/MoneyLoops.cpp
@@ -1,1 +1,36 @@
+#include "core/commands/LoopedCommand.hpp"
+#include "game/gta/Stats.hpp"
+#include "core/backend/ScriptMgr.hpp"
 
+namespace YimMenu::Features
+{
+	class NightclubLoop : public LoopedCommand
+	{
+		using LoopedCommand::LoopedCommand;
+        bool running = false;
+        bool enabled = false;
+        virtual void OnTick() override
+        {
+            if (!running && enabled)
+            {
+                LOGF(INFO, "Nightclub Loop Tick");
+                running = true;
+                Stats::SetInt("MPX_CLUB_POPULARITY", 1000);
+                Stats::SetInt("MPX_CLUB_PAY_TIME_LEFT", 1);
+                ScriptMgr::Yield(std::chrono::seconds(5));
+                running = false;
+            }
+        }
+		virtual void OnEnable() override
+		{
+            enabled = true;
+		}
+
+		virtual void OnDisable() override
+		{
+            enabled = false;
+		}
+	};
+
+	static NightclubLoop _NightclubLoop{"nightclubloop", "Nightclub Loop", "Free money nightclub safe money loop"};
+}
