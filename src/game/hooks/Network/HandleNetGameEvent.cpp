ffb8790be97138af79eb66c52d53d79fbb701505
Hexlane <deede3771@gmail.com>
2026-01-26 08:21:35 -0500

    Update HandleNetGameEvent.cpp

--- src/game/hooks/Network/HandleNetGameEvent.cpp
+++ src/game/hooks/Network/HandleNetGameEvent.cpp
@@ -28,7 +28,7 @@ namespace YimMenu::Hooks
 	{
 		if (*(int16_t*)&event.m_InteriorIndex < -1)
 		{
-			LOG(INFO) << "blocked invalid interior crash from " << player.GetName();
+			g_log.send("INFO", std::format("blocked invalid interior crash from {}", player.GetName()));
 			return true;
 		}
 
@@ -38,14 +38,14 @@ namespace YimMenu::Hooks
 			if (event.m_CameraShake > 0.0f && dist <= 30.0f)
 			{
 				// Camera shake
-				LOGF(WARNING, "Blocked EXPLOSION_EVENT from {} since m_CameraShake > 0.0f", player.GetName());
+				g_log.send("WARNING", std::format("Blocked EXPLOSION_EVENT from {} since m_CameraShake > 0.0f", player.GetName()));
 				return true;
 			}
 
 			if (event.m_DamageScale > 0.0f && dist <= 10.0f)
 			{
 				// Ragdoll
-				LOGF(WARNING, "Blocked EXPLOSION_EVENT from {} since m_DamageScale > 0.0f", player.GetName());
+				g_log.send("WARNING", std::format("Blocked EXPLOSION_EVENT from {} since m_DamageScale > 0.0f", player.GetName()));
 				return true;
 			}
 		}
@@ -138,7 +138,7 @@ namespace YimMenu::Hooks
 
 			if (event.m_WeaponType == "WEAPON_TRANQUILIZER"_J)
 			{
-				LOGF(WARNING, "Blocked WEAPON_DAMAGE_EVENT from {} with m_WeaponType == WEAPON_TRANQUILIZER", player.GetName());
+				g_log.send("WARNING", std::format("Blocked WEAPON_DAMAGE_EVENT from {} with m_WeaponType == WEAPON_TRANQUILIZER", player.GetName()));
 				return false;
 			}
 
@@ -166,7 +166,7 @@ namespace YimMenu::Hooks
 			if (self_veh && self_veh.GetNetworkObjectId() == veh_id && sender_veh && sender_veh.GetNetworkObjectId() != veh_id)
 			{
 				// Vehicle takeover
-				LOGF(WARNING, "Blocked CHANGE_RADIO_STATION_EVENT on our local vehicle from {}", player.GetName());
+				g_log.send("WARNING", std::format("Blocked CHANGE_RADIO_STATION_EVENT on our local vehicle from {}", player.GetName()));
 				return false;
 			}
 
@@ -175,7 +175,7 @@ namespace YimMenu::Hooks
 		case rage::netGameEvent::Type::DOOR_BREAK_EVENT:
 		{
 			// never used for legitimate reasons
-			LOGF(WARNING, "Blocked DOOR_BREAK_EVENT from {}", player.GetName());
+			g_log.send("WARNING", std::format("Blocked DOOR_BREAK_EVENT from {}", player.GetName()));
 			return false;
 		}
 		case rage::netGameEvent::Type::SCRIPTED_GAME_EVENT:
@@ -206,14 +206,14 @@ namespace YimMenu::Hooks
 				if (pop_group == 0 && (percentage == 0 || percentage == 103))
 				{
 					// pop group override crash
-					LOGF(WARNING, "Blocked SCRIPT_WORLD_STATE_EVENT of type PopGroupOverride with invalid params from {}", player.GetName());
+					g_log.send("WARNING", std::format("Blocked SCRIPT_WORLD_STATE_EVENT of type PopGroupOverride with invalid params from {}", player.GetName()));
 					return false;
 				}
 			}
 			else if (type == CScriptWorldStateEvent::Type::PopMultiplierArea && !NETWORK::NETWORK_IS_ACTIVITY_SESSION())
 			{
 				// Stop traffic
-				LOGF(WARNING, "Blocked SCRIPT_WORLD_STATE_EVENT of type PopMultiplierArea from {}", player.GetName());
+				g_log.send("WARNING", std::format("Blocked SCRIPT_WORLD_STATE_EVENT of type PopMultiplierArea from {}", player.GetName()));
 				return false;
 			}
 
@@ -233,14 +233,14 @@ namespace YimMenu::Hooks
 				if (action >= 15 && action <= 18)
 				{
 					// vehicle temp action crash
-					LOGF(WARNING, "Blocked SCRIPT_ENTITY_STATE_CHANGE_EVENT of type SettingOfTaskVehicleTempAction with invalid params from {}", player.GetName());
+					g_log.send("WARNING", std::format("Blocked SCRIPT_ENTITY_STATE_CHANGE_EVENT of type SettingOfTaskVehicleTempAction with invalid params from {}", player.GetName()));
 					return false;
 				}
 
 				if (Self::GetPed().GetNetworkObjectId() == entity)
 				{
 					// Ped takeover
-					LOGF(WARNING, "Blocked SCRIPT_ENTITY_STATE_CHANGE_EVENT of type SettingOfTaskVehicleTempAction on our local ped from {}", player.GetName());
+					g_log.send("WARNING", std::format("Blocked SCRIPT_ENTITY_STATE_CHANGE_EVENT of type SettingOfTaskVehicleTempAction on our local ped from {}", player.GetName()));
 					return false;
 				}
 
@@ -248,7 +248,7 @@ namespace YimMenu::Hooks
 				if (self_veh && self_veh.GetNetworkObjectId() == veh_id)
 				{
 					// Vehicle takeover
-					LOGF(WARNING, "Blocked SCRIPT_ENTITY_STATE_CHANGE_EVENT of type SettingOfTaskVehicleTempAction on our local vehicle from {}", player.GetName());
+					g_log.send("WARNING", std::format("Blocked SCRIPT_ENTITY_STATE_CHANGE_EVENT of type SettingOfTaskVehicleTempAction on our local vehicle from {}", player.GetName()));
 					return false;
 				}
 			}
@@ -257,7 +257,7 @@ namespace YimMenu::Hooks
 				Vehicle self_veh = Self::GetVehicle();
 				if (self_veh && self_veh.GetNetworkObjectId() == entity)
 				{
-					LOGF(WARNING, "Blocked SCRIPT_ENTITY_STATE_CHANGE_EVENT of type SetVehicleLockState from {} on our local vehicle", player.GetName());
+					g_log.send("WARNING", std::format("Blocked SCRIPT_ENTITY_STATE_CHANGE_EVENT of type SetVehicleLockState from {} on our local vehicle", player.GetName()));
 					return false;
 				}
 			}
@@ -267,7 +267,7 @@ namespace YimMenu::Hooks
 				if (self_veh && self_veh.GetNetworkObjectId() == entity)
 				{
 					// Vehicle kick
-					LOGF(WARNING, "Blocked SCRIPT_ENTITY_STATE_CHANGE_EVENT of type SetVehicleExclusiveDriver from {} on our local vehicle", player.GetName());
+					g_log.send("WARNING", std::format("Blocked SCRIPT_ENTITY_STATE_CHANGE_EVENT of type SetVehicleExclusiveDriver from {} on our local vehicle", player.GetName()));
 					return false;
 				}
 			}
@@ -275,7 +275,7 @@ namespace YimMenu::Hooks
 			{
 				if (Self::GetPed().GetNetworkObjectId() == entity)
 				{
-					LOGF(WARNING, "Blocked SCRIPT_ENTITY_STATE_CHANGE_EVENT of type SetPedFacialIdleAnimOverride from {} on our local player", player.GetName());
+					g_log.send("WARNING", std::format("Blocked SCRIPT_ENTITY_STATE_CHANGE_EVENT of type SetPedFacialIdleAnimOverride from {} on our local player", player.GetName()));
 					return false;
 				}
 			}
@@ -294,7 +294,8 @@ namespace YimMenu::Hooks
 
 			if (ScanPlaySoundEvent(event, player))
 			{
-				LOGF(WARNING, "Blocked NETWORK_PLAY_SOUND_EVENT from {} with IsEntity: {}, RefHash: {:X}, SoundHash: {:X}, SoundId: {}, ScriptHash: {:X}", player.GetName(), event.m_IsEntity, event.m_RefHash, event.m_SoundHash, event.m_SoundId, event.m_ScriptId.m_Hash);
+				g_log.send("WARNING", std::format("Blocked NETWORK_PLAY_SOUND_EVENT from {} with IsEntity: {}, RefHash: {:X}, SoundHash: {:X}, SoundId: {}, ScriptHash: {:X}", player.GetName(), event.m_IsEntity, event.m_RefHash, event.m_SoundHash, event.m_SoundId, event.m_ScriptId.m_Hash));
+
 				return false;
 			}
 
@@ -314,7 +315,7 @@ namespace YimMenu::Hooks
 
 			if (cheaterPool->GetState())
 			{
-				LOGF(WARNING, "Blocked REPORT_MYSELF_EVENT from {} with type {} since we're in a cheater-only session", player.GetName(), event.m_0x30);
+				g_log.send("WARNING", std::format("Blocked REPORT_MYSELF_EVENT from {} with type {} since we're in a cheater-only session", player.GetName(), event.m_0x30));
 				return false;
 			}
 
@@ -328,7 +329,7 @@ namespace YimMenu::Hooks
 			if (Self::GetPed().GetVehicleObjectId() == event.m_VehicleId && !Self::GetVehicle().IsRemote())
 			{
 				// Vehicle takeover
-				LOGF(WARNING, "Blocked ACTIVATE_VEHICLE_SPECIAL_ABILITY_EVENT on our local vehicle from {}", player.GetName());
+				g_log.send("WARNING", std::format("Blocked ACTIVATE_VEHICLE_SPECIAL_ABILITY_EVENT on our local vehicle from {}", player.GetName()));
 				return false;
 			}
 
@@ -342,7 +343,7 @@ namespace YimMenu::Hooks
 			if (Self::GetPed().GetNetworkObjectId() == event.m_PedToRagdoll)
 			{
 				// is sometimes used legit, beware
-				LOGF(WARNING, "Blocked RAGDOLL_REQUEST_EVENT on our local ped from {}", player.GetName());
+				g_log.send("WARNING", "Blocked RAGDOLL_REQUEST_EVENT on our local ped from {}", player.GetName());
 				return false;
 			}
 
