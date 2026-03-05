2e4d32b78ca99a237efc45d4b2d89bd3fa470aff
Voidlane <hexlane98@gmail.com>
2026-03-05 10:50:20 -0500

    Update Instant_Finish.cpp

--- src/game/features/recovery/missions/Bunker/Instant_Finish.cpp
+++ src/game/features/recovery/missions/Bunker/Instant_Finish.cpp
@@ -1,5 +1,6 @@
 #include "core/commands/Command.hpp"
 #include "game/gta/ScriptLocal.hpp"
+#include "game/gta/ScriptGlobal.hpp"
 
 namespace YimMenu::Features
 {
@@ -10,6 +11,9 @@ namespace YimMenu::Features
 		virtual void OnCall() override
 		{
 			*ScriptLocal("gb_gunrunning"_J, 1268).At(774).As<int*>() = 0;
+			// High Demand Bonus
+			*ScriptGlobal(262145).At(21232).As<float*>() = 2.5f;
+			*ScriptGlobal(262145).At(21233).As<float*>() = 20.0f;
 		}
 	};
 	static Bunkeri_Sell _Bunkeri_Sell{"Bunker_Instant-Sell","Instant-Finish Bunker","Instant-Finish"};
