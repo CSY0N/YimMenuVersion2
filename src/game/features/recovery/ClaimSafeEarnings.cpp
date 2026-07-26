84dc8277ef8bbbc586313962e92ac2d08f0585e7
Pyxalon <kythera051@gmail.com>
2026-07-25 23:49:18 -0400

    Update ClaimSafeEarnings.cpp

--- src/game/features/recovery/ClaimSafeEarnings.cpp
+++ src/game/features/recovery/ClaimSafeEarnings.cpp
@@ -1,3 +1,4 @@
+#include "core/commands/Command.hpp"
 #include "core/commands/ListCommand.hpp"
 #include "game/backend/Self.hpp"
 #include "game/gta/ScriptGlobal.hpp"
@@ -25,66 +26,138 @@ namespace YimMenu::Features
 	    {static_cast<int>(eAppVinewoodMenuSafe::SALVAGE_YARD), "Salvage Yard"},
 	    {static_cast<int>(eAppVinewoodMenuSafe::BAIL_OFFICE), "Bail Office"},
 	    {static_cast<int>(eAppVinewoodMenuSafe::GARMENT_FACTORY), "Garment Factory"},
-	    {static_cast<int>(eAppVinewoodMenuSafe::HANDS_ON_CAR_WASH), "Hands on Car Wash"}};
+	    {static_cast<int>(eAppVinewoodMenuSafe::HANDS_ON_CAR_WASH), "Hands on Car Wash"}
+	};
 
-	static ListCommand _SelectedBusiness{"businesssafe", "Business", "Business to claim earning from.", businessNames, 0};
+	static ListCommand _SelectedBusiness{
+	    "businesssafe",
+	    "Business",
+	    "Business to claim earnings from.",
+	    businessNames,
+	    0};
+
+	static bool IsValidSession()
+	{
+		return Pointers.IsSessionStarted && *Pointers.IsSessionStarted;
+	}
+
+	static void ClaimNightclubSafe()
+	{
+		const auto playerId = Self::GetPlayer().GetId();
+
+		if (GPBD_FM::Get()->Entries[playerId].PropertyData.NightclubData.SafeCashValue > 0)
+			*ScriptGlobal(2708943).As<BOOL*>() = TRUE;
+	}
+
+	static void ClaimArcadeSafe()
+	{
+		const auto playerId = Self::GetPlayer().GetId();
+
+		if (GPBD_FM::Get()->Entries[playerId].PropertyData.ArcadeData.SafeCashValue > 0)
+			*ScriptGlobal(2708952).As<BOOL*>() = TRUE;
+	}
+
+	static void ClaimAgencySafe()
+	{
+		const auto playerId = Self::GetPlayer().GetId();
+
+		if (GPBD_FM::Get()->Entries[playerId].PropertyData.FixerHQData.SafeCashValue > 0)
+			*ScriptGlobal(2708961).As<BOOL*>() = TRUE;
+	}
+
+	static void ClaimSalvageYardSafe()
+	{
+		const auto playerId = Self::GetPlayer().GetId();
+
+		if (GPBD_FM::Get()->Entries[playerId].PropertyData.SalvageYardData.TotalEarnings > 0)
+			*ScriptGlobal(2708970).As<BOOL*>() = TRUE;
+	}
+
+	static void ClaimBailOfficeSafe()
+	{
+		const auto playerId = Self::GetPlayer().GetId();
+
+		if (GPBD_FM::Get()->Entries[playerId].PropertyData.BailShopData.SafeCashValue > 0)
+			*ScriptGlobal(2708979).As<BOOL*>() = TRUE;
+	}
+
+	static void ClaimGarmentFactorySafe()
+	{
+		const auto playerId = Self::GetPlayer().GetId();
+
+		if (GPBD_FM::Get()->Entries[playerId].PropertyData.HackerDenData.SafeCashValue > 0)
+			*ScriptGlobal(2708994).As<BOOL*>() = TRUE;
+	}
+
+	static void ClaimHandsOnCarWashSafe()
+	{
+		const auto playerId = Self::GetPlayer().GetId();
+
+		if (GPBD_FM_2::Get()->Entries[playerId].SYVehSaleData.HOWCData.SafeCashValue > 0)
+			*ScriptGlobal(2709001).As<BOOL*>() = TRUE;
+	}
 
 	class ClaimSafeEarnings : public Command
 	{
 		using Command::Command;
 
-		virtual void OnCall() override
+		void OnCall() override
 		{
-			if (!*Pointers.IsSessionStarted)
+			if (!IsValidSession())
 				return;
 
 			switch (static_cast<eAppVinewoodMenuSafe>(_SelectedBusiness.GetState()))
 			{
 			case eAppVinewoodMenuSafe::NIGHTCLUB:
-			{
-				if (GPBD_FM::Get()->Entries[Self::GetPlayer().GetId()].PropertyData.NightclubData.SafeCashValue > 0)
-					*ScriptGlobal(2708943).As<BOOL*>() = TRUE;
+				ClaimNightclubSafe();
 				break;
-			}
+
 			case eAppVinewoodMenuSafe::ARCADE:
-			{
-				if (GPBD_FM::Get()->Entries[Self::GetPlayer().GetId()].PropertyData.ArcadeData.SafeCashValue > 0)
-					*ScriptGlobal(2708952).As<BOOL*>() = TRUE;
+				ClaimArcadeSafe();
 				break;
-			}
+
 			case eAppVinewoodMenuSafe::AGENCY:
-			{
-				if (GPBD_FM::Get()->Entries[Self::GetPlayer().GetId()].PropertyData.FixerHQData.SafeCashValue > 0)
-					*ScriptGlobal(2708961).As<BOOL*>() = TRUE;
+				ClaimAgencySafe();
 				break;
-			}
+
 			case eAppVinewoodMenuSafe::SALVAGE_YARD:
-			{
-				if (GPBD_FM::Get()->Entries[Self::GetPlayer().GetId()].PropertyData.SalvageYardData.TotalEarnings > 0)
-					*ScriptGlobal(2708970).As<BOOL*>() = TRUE;
+				ClaimSalvageYardSafe();
 				break;
-			}
+
 			case eAppVinewoodMenuSafe::BAIL_OFFICE:
-			{
-				if (GPBD_FM::Get()->Entries[Self::GetPlayer().GetId()].PropertyData.BailShopData.SafeCashValue > 0)
-					*ScriptGlobal(2708979).As<BOOL*>() = TRUE;
+				ClaimBailOfficeSafe();
 				break;
-			}
+
 			case eAppVinewoodMenuSafe::GARMENT_FACTORY:
-			{
-				if (GPBD_FM::Get()->Entries[Self::GetPlayer().GetId()].PropertyData.HackerDenData.SafeCashValue > 0)
-					*ScriptGlobal(2708994).As<BOOL*>() = TRUE;
+				ClaimGarmentFactorySafe();
 				break;
-			}
+
 			case eAppVinewoodMenuSafe::HANDS_ON_CAR_WASH:
-			{
-				if (GPBD_FM_2::Get()->Entries[Self::GetPlayer().GetId()].SYVehSaleData.HOWCData.SafeCashValue > 0)
-					*ScriptGlobal(2709001).As<BOOL*>() = TRUE;
+				ClaimHandsOnCarWashSafe();
 				break;
-			}
 			}
 		}
 	};
 
-	static ClaimSafeEarnings _ClaimSafeEarnings{"claimsafeearnings", "Claim Safe Earnings", "Claims your safe earnings from the selected business."};
+	class ClaimAllSafeEarnings : public Command
+	{
+		using Command::Command;
+
+		void OnCall() override
+		{
+			if (!IsValidSession())
+				return;
+
+			ClaimNightclubSafe();
+			ClaimArcadeSafe();
+			ClaimAgencySafe();
+			ClaimSalvageYardSafe();
+			ClaimBailOfficeSafe();
+			ClaimGarmentFactorySafe();
+			ClaimHandsOnCarWashSafe();
+		}
+	};
+
+	static ClaimSafeEarnings _ClaimSafeEarnings{"claimsafeearnings", "Claim Safe Earnings", "Claims safe earnings from the selected business."};
+	static ClaimAllSafeEarnings _ClaimAllSafeEarnings{"claimallsafeearnings", "Claim All Safe Earnings", "Claims available safe earnings from every owned business."};
 }
