3774b8b69d1c272e99ac8c7fe9d58552d2e1327c
Pyxalon <kythera051@gmail.com>
2026-08-12 20:03:37 -0400

    Update Cooldown.cpp

--- src/game/features/recovery/Cooldown.cpp
+++ src/game/features/recovery/Cooldown.cpp
@@ -60,7 +60,7 @@ namespace YimMenu::Features
 			if (*Pointers.IsSessionStarted)
 			{
 				FiberPool::Push([] {
-					ScriptGlobal(2686095).At(4357).At(260).At(7, 2).As<TIMER*>()->Destroy();
+					ScriptGlobal(2686124).At(4373).At(260).At(7, 2).As<TIMER*>()->Destroy();
 				});
 				STATS::STAT_SAVE(0, 0, 3, 0);
 			}
