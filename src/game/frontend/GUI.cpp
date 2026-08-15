bb1ae951e31f04178c1c162a8ecae43319457212
Pyxalon <kythera051@gmail.com>
2026-08-15 14:58:26 -0400

    Update GUI.cpp

--- src/game/frontend/GUI.cpp
+++ src/game/frontend/GUI.cpp
@@ -26,39 +26,39 @@ namespace YimMenu
 			GUI::WndProc(hwnd, msg, wparam, lparam);
 		});
 
-		Renderer::AddRendererCallBack(
+		Renderer::AddRendererCallback(
 		    [&] {
 			    Notifications::Draw();
 		    },
 		    -2);
-		Renderer::AddRendererCallBack(
+		Renderer::AddRendererCallback(
 		    [&] {
 			    ESP::Draw();
 		    },
 		    -3);
-		Renderer::AddRendererCallBack(
+		Renderer::AddRendererCallback(
 		    [&] {
 			    ChatDisplay::Draw();
 		    },
 		    -5);
-		Renderer::AddRendererCallBack(
+		Renderer::AddRendererCallback(
 		    [&] {
 			    Overlay::Draw();
 		    },
 		    -6);
-		Renderer::AddRendererCallBack(
+		Renderer::AddRendererCallback(
 		    [&] {
 			    AutoDriveHUD::Draw();
 		    },
 		    -7);
-		Renderer::AddRendererCallBack(
+		Renderer::AddRendererCallback(
 		    [] {
 			    LuaManager::ForAllLoadedScripts([](std::shared_ptr<LuaScript>& script) {
 				    script->GetUserInterface().DrawAlwaysDrawImGuiCallbacks();
 			    });
 		    },
 		    -8);
-		Renderer::AddRendererCallBack(
+		Renderer::AddRendererCallback(
 		    [] {
 			    if (!GUI::IsOpen())
 				    return;
@@ -69,10 +69,6 @@ namespace YimMenu
 		    -4);
 
 		Renderer::SetSafeToRender();
-	}
-
-	GUI::~GUI()
-	{
 	}
 
 	void GUI::ToggleMouse()
