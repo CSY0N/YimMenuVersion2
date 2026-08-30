c0719b91ab84ae79a74151e8911a200edd985f38
Pyxalon <kythera051@gmail.com>
2026-08-30 14:10:12 -0400

    Update NewYears.cpp

--- src/game/features/recovery/unlocks/NewYears.cpp
+++ src/game/features/recovery/unlocks/NewYears.cpp
@@ -2,7 +2,7 @@
 #include "game/gta/ScriptGlobal.hpp"
 #include "core/frontend/Notifications.hpp"
 #include "game/pointers/Pointers.hpp"
-
+#include "core/backend/FiberPool.hpp"
 
 namespace YimMenu::Features
 {
@@ -24,8 +24,29 @@ namespace YimMenu::Features
 		}
 	};
 
-	static EnableNewYearsGifts _EnableNewYearsGifts{
-	    "enable_new_years_gifts",
-	    "Enable New Years Gifts",
-	    "Enables 2022 & 2023 New Years gift tunables"};
+	static EnableNewYearsGifts _EnableNewYearsGifts{"enable_new_years_gifts", "Enable New Years Gifts", "Enables 2022 & 2023 New Years gift tunables"};
+}
+namespace YimMenu::Features
+{
+	class EnableLunarNewYear : public Command
+	{
+		using Command::Command;
+
+		void OnCall() override
+		{
+			if (*Pointers.IsSessionStarted)
+			{
+				FiberPool::Push([] {
+					*ScriptGlobal(262145).At(36741).As<int*>() = 1;
+					*ScriptGlobal(262145).At(34048).As<int*>() = 1;
+				});
+			}
+			else
+			{
+				Notifications::Show("YimMenuV2", "You must be online", NotificationType::Error);
+			}
+		}
+	};
+
+	static EnableLunarNewYear _EnableLunarNewYear{"enable_lunar_new_year", "Enable Lunar New Year", "Enables the Lunar New Year Yuanbao collectible event"};
 }
