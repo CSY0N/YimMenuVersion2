b56125a145a8e5dad306856c6bdefa81e3d29a3f
Pyxalon <kythera051@gmail.com>
2026-09-04 21:28:22 -0400

    Update Heal.cpp

--- src/game/features/self/Heal.cpp
+++ src/game/features/self/Heal.cpp
@@ -1,4 +1,5 @@
 #include "core/commands/Command.hpp"
+#include "core/commands/LoopedCommand.hpp"
 #include "game/backend/Self.hpp"
 
 namespace YimMenu::Features
@@ -11,10 +12,26 @@ namespace YimMenu::Features
 		{
 			int maxHealth = Self::GetPed().GetMaxHealth();
 			int maxArmour = Self::GetPlayer().GetMaxArmour();
+
 			Self::GetPed().SetHealth(maxHealth);
 			Self::GetPed().SetArmour(maxArmour);
 		}
 	};
 
+	class AutoHeal : public LoopedCommand
+	{
+		using LoopedCommand::LoopedCommand;
+
+		virtual void OnTick() override
+		{
+			if (!Self::GetPed())
+				return;
+
+			if (Self::GetPed().GetHealth() < Self::GetPed().GetMaxHealth())
+				Self::GetPed().SetHealth(Self::GetPed().GetMaxHealth());
+		}
+	};
+
 	static Heal _Heal{"heal", "Heal", "Restores your health and armour."};
+	static AutoHeal _AutoHeal{"autoheal", "Auto Heal", "Automatically restores your health when it decreases."};
 }
