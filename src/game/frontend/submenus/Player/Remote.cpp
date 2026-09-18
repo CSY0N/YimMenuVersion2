d4c11b4e07b2d58fee63cdd24623b17871cfe68c
Pyxalon <kythera051@gmail.com>
2026-09-18 17:04:28 -0400

    Create Remote.cpp

---
+++ src/game/frontend/submenus/Player/Remote.cpp
@@ -0,0 +1,22 @@
+#include "Remote.hpp"
+
+namespace YimMenu::Submenus
+{
+    std::shared_ptr<Category> BuildRemoteMenu()
+    {
+        auto menu = std::make_shared<Category>("Remote");
+
+        auto actions = std::make_shared<Group>("Actions", 1);
+        actions->AddItem(std::make_shared<PlayerCommandItem>("ceokick"_J));
+        actions->AddItem(std::make_shared<PlayerCommandItem>("sendpizza"_J));
+
+        auto notifications = std::make_shared<Group>("Notifications");
+        notifications->AddItem(std::make_shared<ListCommandItem>("notificationtype"_J));
+        notifications->AddItem(std::make_shared<PlayerCommandItem>("sendnotification"_J));
+
+        menu->AddItem(actions);
+        menu->AddItem(notifications);
+
+        return menu;
+    }
+}
