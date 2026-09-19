7990acc32d3a01143b6a294058ebc45da0bab6c3
Pyxalon <kythera051@gmail.com>
2026-09-18 22:47:41 -0400

    Add missions group to player remote menu

--- src/game/frontend/submenus/Player/Remote.cpp
+++ src/game/frontend/submenus/Player/Remote.cpp
@@ -10,11 +10,17 @@ namespace YimMenu::Submenus
         actions->AddItem(std::make_shared<PlayerCommandItem>("ceokick"_J));
         actions->AddItem(std::make_shared<PlayerCommandItem>("sendpizza"_J));
 
+        auto missions = std::make_shared<Group>("Force Mission");
+        missions->AddItem(std::make_shared<ListCommandItem>("forcemissiontype"_J));
+        missions->AddItem(std::make_shared<PlayerCommandItem>("forcemission"_J));
+
+
         auto notifications = std::make_shared<Group>("Notifications");
         notifications->AddItem(std::make_shared<ListCommandItem>("notificationtype"_J));
         notifications->AddItem(std::make_shared<PlayerCommandItem>("sendnotification"_J));
 
         menu->AddItem(actions);
+		menu->AddItem(missions);
         menu->AddItem(notifications);
 
         return menu;
